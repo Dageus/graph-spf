0088ebdb1c0f056cc4893986fb2373b1ac66049d
Dageus <jomouzio@gmail.com>
2022-12-21 17:25:46 +0000

    simple code

--- proj2.cpp
+++ proj2.cpp
@@ -1,1 +1,23 @@
-//make code
+//include the header files
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <bits/stdc++.h>
+#include <vector>
+
+// declare macros
+
+
+// declare global variables
+
+
+// declare functions
+
+
+// main function
+int main() {
+
+    return 0;
+}
