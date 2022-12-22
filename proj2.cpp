c5ee4a81651f22e3c804b7ab78425f4a7925bb63
Dageus <jomouzio@gmail.com>
2022-12-22 15:57:01 +0000

    some logic for the code

--- proj2.cpp
+++ proj2.cpp
@@ -6,18 +6,48 @@
 #include <fstream>
 #include <bits/stdc++.h>
 #include <vector>
+using namespace std;
+
+// struct for the graph
+struct Graph{
+    int V, E;
+    vector<int> adj;
+};
+
+// struct for the weight
+struct Weight{
+    int vertice;
+    int weight;
+};
 
 // declare macros
 
 
 // declare global variables
+int V, E;
+// find graph representation
+// -> use list of adjacencies <-
+vector<int> adj;
 
 
 // declare functions
+void readInput(){
+    cin >> V;
+    cin >> E;
+
+    for(int i = 0; i < E; i++){
+        int u, v, w;
+        cin >> u;
+        cin >> v;
+        cin >> w;
+        cout << "arco (" << u << ", " << v << ") com peso " << w << endl;
+    }
+}
 
 
 // main function
 int main() {
+    readInput();
 
     return 0;
 }
